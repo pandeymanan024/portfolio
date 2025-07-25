33700c584ff46a2dc7c51cc233757514f8c0f4e8
Manan Pandey <pandeymanan024@gmail.com>
2025-07-25 15:19:14 -0400

    variable issue fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,15 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-sans bg-blue-50 min-h-screen">
-        {children},
-        [geistMono],
-        [geistSans]
+        {children}
       </body>
     </html>
   );
